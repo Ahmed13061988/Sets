39d180607472f929bca3028abb724efd28203427
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-07 09:58:45 -0600

    second

---
+++ script.js
@@ -0,0 +1,12 @@
+// Sets are a collection of unique values, that's mean there is no duplicates in it.
+
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(ordersSet);
